2047863776e5f825fbedd004973e4bdeb4d72c41
ahmadrosid <alahmadrosid@gmail.com>
2022-02-20 11:08:14 +0700

    feat: redesign the ui

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Router from 'next/router'
 import { useEffect, useRef } from 'react'
+import { BiSearchAlt } from "react-icons/bi"
 
 export default function Home() {
   const inputRef = useRef();
@@ -22,24 +23,41 @@ export default function Home() {
         <script defer data-domain="heline.dev" src="https://plausible.io/js/plausible.js"></script>
       </Head>
 
-      <div className="grid place-items-center w-full h-screen">
+      <div className="max-w-7xl min-h-[calc(100vh-4.75rem)] mx-auto py-14 sm:py-20 md:py-24 px-4 sm:px-6 lg:px-8 sm:flex sm:flex-col sm:items-center">
         <div className="text-center space-y-4 w-full max-w-xl p-8">
-          <h1 className="text-6xl font-bold uppercase text-green-500">Heline</h1>
-          <p className="text-lg font-normal text-gray-500">
-            Code search for function names, code snippets etc 🚀.
+          <h1 className="text-5xl font-extrabold text-gray-900 dark:text-white sm:text-center inline-flex items-center select-none">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-teal-600">
+              heline
+            </span>
+            <span>.</span>
+            <span className="text-gray-700">dev</span>
+          </h1>
+          <p className="text-lg font-normal text-gray-400">
+            Search Engine for Modern Developers.
           </p>
-          <div className="py-6">
-            <input
-              ref={inputRef}
-              onChange={handleOnChange}
-              type="text"
-              placeholder="Search"
-              className="px-5 py-3 rounded-lg text-md w-full border-[1.5px] border-green-600 focus:outline-none"
-            />
+          <div className="self-center py-2 mt-8 bg-white dark:bg-black shadow-md w-full max-w-xl rounded overflow-hidden content border">
+            <div className="px-4 py-2 font-mono flex items-center">
+              <span className="text-gray-500 select-none text-xl pr-3"><BiSearchAlt /></span>
+              <input
+                ref={inputRef}
+                onChange={handleOnChange}
+                type="text"
+                name="search-string"
+                id="search-string"
+                autoFocus
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="off"
+                spellCheck="false"
+                placeholder="Search for function, variable, snippets etc."
+                className="flex-grow text-gray-900 dark:bg-black dark:text-white border-none outline-none focus:outline-none focus:ring-0 autofill:shadow-fill-white dark:autofill:shadow-fill-black"
+                aria-label="search"
+              />
+            </div>
           </div>
           <div className="text-gray-500">
             Created by <a
-              className="text-green-500 font-medium hover:underline"
+              className="text-emerald-500 font-medium hover:underline"
               target="_blank"
               href="https://ahmadrosid.com">@_ahmadrosid</a
             >
--- ui/pages/search.js
+++ ui/pages/search.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import useDebounce from '../lib/useDebounce'
 import axios from 'axios'
 import renderArray from '../lib/render-array'
+import { BiSearchAlt } from "react-icons/bi"
 
 export default function Home() {
     const router = useRouter()
@@ -57,17 +58,17 @@ export default function Home() {
         if (filter?.repo?.length > 0) {
             queryParam += `&filter[repo]=${filter.repo.join(",")}`
         }
-        
+
         if (filter?.lang?.length > 0) {
             queryParam += `&filter[lang]=${filter.lang.join(",")}`
         }
-        
+
         if (filter?.path?.length > 0) {
             queryParam += `&filter[path]=${filter.path.join(",")}`
         }
-        
+
         axios.get(`https://heline.dev/api/search?q=${queryParam}`)
-        // axios.get(`/api/search?q=${queryParam}`)
+            // axios.get(`/api/search?q=${queryParam}`)
             .then(res => {
                 if (res.data.hits.hits === null) {
                     setNotFound(true)
@@ -109,7 +110,7 @@ export default function Home() {
                 fetchData(val, newFilter)
                 return newFilter
             }
-            
+
             newFilter[filterName]?.push(filter)
             fetchData(val, newFilter)
             return newFilter
@@ -155,25 +156,35 @@ export default function Home() {
     }, [q])
 
     return (
-        <div className="bg-[#f7f6f3] min-h-screen h-full">
+        <div className="bg-zinc-50 min-h-screen h-full">
             <Head>
                 <meta name='viewport' content='width=device-width,initial-scale=1' />
                 <title>{`${q} - heline`}</title>
                 <link rel='icon' type='image/png' href='/favicon.png' />
                 <script defer data-domain="heline.dev" src="https://plausible.io/js/plausible.js"></script>
             </Head>
-            <nav className="border-b bg-white border-green-300">
+            <nav className="bg-white shadow-sm">
                 <div className="py-6 w-full max-w-7xl mx-auto flex items-center gap-4">
                     <div className="w-full max-w-[25%]">
-                        <div className="text-green-500 flex items-center gap-x-2 px-4">
-                            <img src="/favicon.png" className="w-8" />
-                            <a href="/" className="tracking-wide font-medium uppercase text-3xl pb-1">heline</a>
+                        <div className="text-emerald-500 flex items-center gap-x-2 px-4">
+                            {/* <img src="/favicon.png" className="w-8" /> */}
+                            <a href="/" >
+                                {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-teal-600">
+                                    heline
+                                </span> */}
+                                <h1 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-center inline-flex items-center select-none">
+                                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-teal-600">
+                                        heline
+                                    </span>
+                                    <span>.</span>
+                                    <span className="text-gray-700">dev</span>
+                                </h1>
+                            </a>
                         </div>
                     </div>
-                    <div className="flex px-4 rounded-lg border border-green-400 bg-white items-center justify-between w-full mr-8 mx-4">
-                        <svg viewBox="64 64 896 896" focusable="false" className="text-gray-400" data-icon="search" width="1em" height="1em" fill="currentColor" aria-hidden="true">
-                            <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-                        </svg>
+                    <div className="flex px-4 rounded shadow-sm border bg-white items-center justify-between w-full mr-8 mx-4">
+                        <span className="text-gray-500 select-none text-xl pr-4"><BiSearchAlt /></span>
+
                         <input
                             onChange={(el) => {
                                 setVal(encodeURIComponent(el.target.value))
@@ -183,14 +194,14 @@ export default function Home() {
                             defaultValue={q}
                             type="text"
                             placeholder="Search"
-                            className="text-md bg-white rounded-full w-full focus:outline-none py-3 px-2 text-gray-500"
+                            className="py-3 flex-grow text-gray-900 dark:bg-black dark:text-white border-none outline-none focus:outline-none focus:ring-0 autofill:shadow-fill-white dark:autofill:shadow-fill-black"
                         />
                     </div>
                 </div>
             </nav>
             {notFound && (
                 <div className="grid place-items-center pt-32 space-y-8">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 text-emerald-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
                     </svg>
                     <div className="text-center text-lg text-gray-600">
@@ -206,50 +217,50 @@ export default function Home() {
                             <h3 className="text-gray-800 uppercase">Repository</h3>
                             {/* <input className="bg-white rounded border border-gray-200 w-full px-4 py-3 text-sm" type="text" placeholder="Filter repositories" /> */}
                             <div className="py-2 space-y-1">
-                            {renderArray(
-                                hits.facets?.repo?.buckets.map((item, index) => (
-                                    <div className="flex justify-between items-center text-gray-600 pr-1">
-                                        <div className="flex gap-2 items-center">
-                                            <input onChange={() => updateFilterRepo(index)} id={item.val} className="p-2" type="checkbox" checked={filter.repo?.includes(item.val)}/>
-                                            <label htmlFor={item.val}>{item.val}</label>
+                                {renderArray(
+                                    hits.facets?.repo?.buckets.map((item, index) => (
+                                        <div className="flex justify-between items-center text-gray-600 pr-1">
+                                            <div className="inline-flex gap-2 items-center truncate">
+                                                <input onChange={() => updateFilterRepo(index)} id={item.val} className="p-2" type="checkbox" checked={filter.repo?.includes(item.val)} />
+                                                <label className='truncate' htmlFor={item.val}>{item.val}</label>
+                                            </div>
+                                            <div className="text-sm">{item.count}</div>
                                         </div>
-                                        <div className="text-sm">{item.count}</div>
-                                    </div>
-                                ))
-                            )}
+                                    ))
+                                )}
                             </div>
                         </div>
                         <div className="space-y-2 pt-2">
                             <h3 className="text-gray-800 uppercase">Path</h3>
                             {/* <input className="bg-white rounded border border-gray-200 w-full px-4 py-3 text-sm" type="text" placeholder="Filter paths" /> */}
                             <div className="py-2 space-y-1">
-                            {renderArray(
-                                hits.facets?.path?.buckets.map((item, index) => (
-                                    <div className="flex justify-between items-center text-gray-600 pr-1">
-                                        <div className="flex gap-2 items-center">
-                                            <input onChange={() => updateFilterPath(index)} className="p-2" type="checkbox" checked={filter.path?.includes(item.val)}/>
-                                            <label className="truncate">{getPath(item.val)}</label>
+                                {renderArray(
+                                    hits.facets?.path?.buckets.map((item, index) => (
+                                        <div className="flex justify-between items-center text-gray-600 pr-1">
+                                            <div className="flex gap-2 items-center truncate">
+                                                <input onChange={() => updateFilterPath(index)} className="p-2" type="checkbox" checked={filter.path?.includes(item.val)} />
+                                                <label className='truncate'>{getPath(item.val)}</label>
+                                            </div>
+                                            <div className="text-sm">{item.count}</div>
                                         </div>
-                                        <div className="text-sm">{item.count}</div>
-                                    </div>
-                                ))
-                            )}
+                                    ))
+                                )}
                             </div>
                         </div>
                         <div className="space-y-2 pt-2">
                             <h3 className="text-gray-800 uppercase">Language</h3>
                             <div className="py-2 space-y-1">
-                            {renderArray(
-                                hits.facets?.lang?.buckets.map((item, index) => (
-                                    <div className="flex justify-between items-center text-gray-600 pr-1">
-                                        <div className="flex gap-2 items-center">
-                                            <input onChange={() => updateFilterLang(index)} className="p-2" type="checkbox" checked={filter.lang?.includes(item.val)} />
-                                            <label>{item.val}</label>
+                                {renderArray(
+                                    hits.facets?.lang?.buckets.map((item, index) => (
+                                        <div className="flex justify-between items-center text-gray-600 pr-1">
+                                            <div className="flex gap-2 items-center">
+                                                <input onChange={() => updateFilterLang(index)} className="p-2" type="checkbox" checked={filter.lang?.includes(item.val)} />
+                                                <label>{item.val}</label>
+                                            </div>
+                                            <div className="text-sm">{item.count}</div>
                                         </div>
-                                        <div className="text-sm">{item.count}</div>
-                                    </div>
-                                ))
-                            )}
+                                    ))
+                                )}
                             </div>
                         </div>
                     </div>
@@ -272,10 +283,10 @@ export default function Home() {
                                             </a>
 
                                             <a target="_blank" href={`https://github.com/${item.repo.raw}/blob/${item.branch.raw}/${item.file_id.raw.split("/").slice(4, 100).join("/")}`} className="flex gap-1 items-center">
-                                                <span className="text-green-500 pl-8">{item.file_id.raw.split("/").slice(2, 100).join("/")}</span>
+                                                <span className="text-emerald-500 pl-8 truncate">{item.file_id.raw.split("/").slice(2, 100).join("/")}</span>
                                             </a>
                                         </div>
-                                        <div className="border border-gray-200 rounded-md bg-white p-2 my-2">
+                                        <div className="border border-zinc-200 rounded-md bg-white p-2 my-2">
                                             {renderArray(
                                                 item.content.snippet?.map((content, parentIndex) => {
                                                     if (content.length === 0) return;
@@ -299,7 +310,7 @@ export default function Home() {
                                                         contents.push(chunk)
                                                     }
 
-                                                    if (contents.length == 0)return;
+                                                    if (contents.length == 0) return;
 
                                                     // Take only 3 element to render!
                                                     contents = contents.slice(0, 3);
@@ -313,7 +324,7 @@ export default function Home() {
                                                                             <table className="highlight-table">
                                                                                 <tbody dangerouslySetInnerHTML={{ __html: source }}></tbody>
                                                                             </table>
-                                                                            { (i < contents.length && parentIndex < (item.content.snippet.length - 1)) && <div className="bg-[#f1f0ec] h-8 my-[8px] -mx-2"></div>}
+                                                                            {(i < contents.length && parentIndex < (item.content.snippet.length - 1)) && <div className="bg-zinc-100 h-8 my-[8px] -mx-2"></div>}
                                                                         </>
                                                                     )
                                                                 })
