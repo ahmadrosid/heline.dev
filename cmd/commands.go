c556a3d1b7cea91e7a0c08fa4a9b9a53d244f482
ahmadrosid <alahmadrosid@gmail.com>
2022-03-06 15:14:55 +0700

    feat: remove scraping code

--- cmd/commands.go
+++ cmd/commands.go
@@ -11,12 +11,6 @@ func Run(args []string) int {
 	c := cli.NewCLI("heline", "1.0.0")
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
-		"scrape github": func() (cli.Command, error) {
-			return &GithubCrawlerCommand{}, nil
-		},
-		"scrape docset": func() (cli.Command, error) {
-			return &GithubCrawlerCommand{}, nil
-		},
 		"server": func() (cli.Command, error) {
 			return &ServerCommand{}, nil
 		},
