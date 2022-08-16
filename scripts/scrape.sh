0c8c2b5b1f3741ac9f06bec9ee2813530abaa3d6
Ahmad Rosid <alahmadrosid@gmail.com>
2022-08-16 22:45:24 +0700

    feat: update scraper script

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-cd _build/heline-indexer
-
 array=(
+  "gitlab.json"
   "sh.json"
-  # "tokio-rs/tokio"
-  # "rails/rails"
-  # "laravel/framework"
-  # "laravel/laravel"
-  # "hashicorp/vault"
-  # "hashicorp/nomad"
-  # "hashicorp/vagrant"
-  # "hashicorp/terraform"
-  # "hashicorp/consul"
-  # "navidrome/navidrome"
-  # "bitwarden/web"
-  # "hairyhenderson/gomplate"
-  # "gookit/goutil"
-  # "sharkdp/bat"
-  # "TheAlgorithms/Go"
-  # "TheAlgorithms/Javascript"
-  # "TheAlgorithms/C-Plus-Plus"
-  # "TheAlgorithms/Python"
-  # "TheAlgorithms/Java"
 )
+
 for i in "${array[@]}"
 do
 	BASE_URL=http://localhost:8984 hli -- $i;
